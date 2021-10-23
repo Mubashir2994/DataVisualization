ae2e8ac8bb709bed09cc976eef7940e8aa3ce63c
mubashir2994 <ali.mubashir2993@yahoo.com>
2021-10-23 17:00:58 -0400

    Cleaned CSV file uploaded and edited code for sending json data

--- Project/BackEnd/index.js
+++ Project/BackEnd/index.js
@@ -1,5 +1,12 @@
+// import * as fs from 'fs';
+// import * as fs from 'fs/promises';
+ 
+// import * as d3 from 'd3'
+
 const express=require('express');  // importing express using require
-  const app = express(); //Creating an instance of express
+const app = express(); //Creating an instance of express
+const fs = require('fs')
+//const d3 = require("d3");
 
 app.get('/',(req,res)=>{
     res.send('<h2 style="margin:2rem"> Welcome to Homepage</h2>');
@@ -8,6 +15,25 @@ app.get('/',(req,res)=>{
 app.get('/about',(req,res)=>{
     res.send('<h2 style="margin:2rem"> Welcome to About us page</h2>');
 })
+
+app.get('/data',(req,res)=>{
+    res.send(data);
+})
+
+app.get("/list_movies", (req, res) => {
+    fs.readFile(__dirname + '/' + 'movies.json', 'utf8', (err, data) => {
+        res.send(data);
+    });
+});
+
+
+var data = fs.readFileSync("C02_EmissionCleaned.csv", "utf8")
+//z= function (data) {return {xyz :data}}
+console.log(data)
+
+// d3.csv('C02_EmissionCleaned.csv').then(data=>{
+//     console.log("hello")
+// })
 
 const port = process.env.PORT || 3000 //First inititalizing the port
 
